7352eb992b5918a4f7ebb58385313764ace653ad
den4ikos <denasius89@yandex.ru>
2023-02-12 12:42:20 +0300

    1. added entity named Hit 2. added custom validation and annotation for validate ip address

---
+++ statistics/src/main/java/ru/practicum/explorewithme/entity/Hit.java
@@ -0,0 +1,29 @@
+package ru.practicum.explorewithme.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.explorewithme.annotation.IpCheckConstraint;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "hits")
+public class Hit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "App is required!")
+    private String app;
+
+    @NotNull(message = "Uri is required!")
+    private String uri;
+
+    @IpCheckConstraint
+    private String ip;
+}
---
+++ statistics/src/main/java/ru/practicum/explorewithme/validation/IpValidator.java
@@ -0,0 +1,27 @@
+package ru.practicum.explorewithme.validation;
+
+import ru.practicum.explorewithme.annotation.IpCheckConstraint;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class IpValidator implements ConstraintValidator<IpCheckConstraint, String> {
+    @Override
+    public void initialize(IpCheckConstraint constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(String field, ConstraintValidatorContext cxt) {
+        if (field == null) return false;
+
+        String zeroTo255 = "(\\d{1,2}|(0|1)\\" + "d{2}|2[0-4]\\d|25[0-5])";
+        String regex = zeroTo255 + "\\."+ zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255;
+        Pattern p = Pattern.compile(regex);
+
+        Matcher m = p.matcher(cxt.toString());
+
+        return m.matches();
+    }
+}
