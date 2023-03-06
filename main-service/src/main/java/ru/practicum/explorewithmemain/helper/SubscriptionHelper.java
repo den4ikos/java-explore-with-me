ae24b7f937465a21d3847160160a2e500a84320a
den4ikos <denasius89@yandex.ru>
2023-03-06 22:02:28 +0300

    fixes: added javadoc for the SubscriptionHelper class

--- main-service/src/main/java/ru/practicum/explorewithmemain/helper/SubscriptionHelper.java
+++ main-service/src/main/java/ru/practicum/explorewithmemain/helper/SubscriptionHelper.java
@@ -3,7 +3,16 @@ package ru.practicum.explorewithmemain.helper;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Class helper for subscriptions
+ * @author den4ikos
+ */
 public class SubscriptionHelper {
+
+    /**
+     * @return all allowed states for the published event
+     * @see State
+     */
     public static List<State> getAllowedStates() {
         State[] statesArray = new State[]{State.PUBLISHED, State.PUBLISH_EVENT};
         return Arrays.asList(statesArray);
